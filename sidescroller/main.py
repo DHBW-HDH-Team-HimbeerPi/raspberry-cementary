46782b22c9d40905973ae0b25f79c3a134db434a
Maximilian Volk <maximilian.volk@daimler.com>
2022-01-12 16:07:18 +0100

    text output

--- sidescroller/main.py
+++ sidescroller/main.py
@@ -1,10 +1,39 @@
 import numpy as np
+#from OutputFramework.OutFramework import OutputFramework as of
+
+print("hello")
+
+def show(ausgabe: list):
+    length = len(ausgabe)
+    for x in range(length):
+        for y in range(length):
+            printed = 0
+            for color in range(3):
+                if(ausgabe[x][y][color] != 0):
+                    print("0", end =" ")
+                    break
+                else:
+                    if (printed == 0):
+                        print("_", end =" ")
+                        printed = 1
+        print("")
+
+pixels = 3
+pixelArray = np.full((pixels , pixels, 3), 0)
+
+pixelArray[1][1][0] = 3
+
+show(pixelArray)
 
 
-pixels = 5
-pixelArray = np.full((4, pixels, pixels), 0)
 
-print(pixelArray)
+            
+                
+
+
+
+
+#show(pixelArray)
 
 
 
