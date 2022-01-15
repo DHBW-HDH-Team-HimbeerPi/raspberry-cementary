597b909ddacaffa10c97a8c1115663f2400498cb
tinvol <maximilian.volk@daimler.com>
2022-01-15 14:20:51 +0100

    folder cleanup

--- sidescroller/main.py
+++ sidescroller/main.py
@@ -1,15 +1,14 @@
 import os
 import time
 import numpy as np
-from devShow import show
-from shiftPixels import shiftPixels
-from addObject import add
-from player import Player
+from src.devShow import show
+from src.shiftPixels import shiftPixels
+from src.addObject import add
+from src.player import Player
 #from output_framework.output_framework import OutputFramework
 
 PIXELS = 16
 pixelArray = np.full((PIXELS , PIXELS, 4), 0)
-running = True
 pixelArray[10][10][0] = 255
 
 def main():
@@ -17,16 +16,17 @@ def main():
     joe = Player(PIXELS)
     add(pixelArray, joe.dimensions())
     #show(pixelArray)
+    running = True
 
     while running:
         #events
             #input
         #update
-        shiftPixels(pixelArray)
+        running = shiftPixels(pixelArray)
         #draw
-        #show(pixelArray)
+        show(pixelArray)
         #OutputFramework.setWindow(pixelArray)
-        time.sleep(0.5) 
+        time.sleep(5) 
         #os.system('cls')
 
 if __name__ == "__main__":
--- sidescroller/shiftPixels.py
+++
@@ -1,5 +0,0 @@
-def shiftPixels(pixelArray: list):
-    for x in range(len(pixelArray)):
-        for y in range(1, len(pixelArray[0])):
-            if pixelArray[x][y-1][3] == 0 and pixelArray[x][y][3] == 0:
-                pixelArray[x][y-1] = pixelArray[x][y]
