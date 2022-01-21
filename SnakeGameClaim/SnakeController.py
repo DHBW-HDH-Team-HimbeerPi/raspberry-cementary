49668795e00b67e17856555c5f1766f021793033
PercyWithoutMercy <noahliberka@gmail.com>
2022-01-21 12:04:54 +0100

    AppleController and more stuff added

--- SnakeGameClaim/SnakeController.py
+++ SnakeGameClaim/SnakeController.py
@@ -1,32 +1,41 @@
-from http.client import SWITCHING_PROTOCOLS
+from xmlrpc.client import boolean
 import numpy as np
 
 class SnakeController:
 
+    pixelAmount = 16
+    pixelArray = np.full((pixelAmount , pixelAmount, 3), 0)
+    posX = 0
+    posY = 0
+    posXOld = 0
+    posYOld = 0
+
     def __init__(self, posX, posY):
-        colorR = 0
-        colorG = 255
-        colorB = 0
         self.posX = posX
         self.posY = posY
+        self.posXOld = 0
+        self.posYOld = 0
 
     def MoveSnake(self, direction: int):
-        # match direction:
-        #     case 1:
-        #         self.posY += 1
-        #     case 2:
-        #         self.posY -= 1
-        #     case 3: 
-        #         self.posX += 1
-        #     case 4:
-        #         self.posX -= 1
-                
+        self.posYOld = self.posY
+        self.posXOld = self.posX
+        
         if (direction == 1):
-            self.posY = 1
+            self.posY += 1
         elif (direction == 2):
-            self.posY = -1
+            self.posY -= 1
         elif (direction == 3):
-            self.posX = 1
+            self.posX -= 1
         elif (direction == 4):
-            self.posX = -1
+            self.posX += 1
+
+    def SnakeIsAlive(self):
+        if ((self.posY >= 15 or self.posY <= 0) or (self.posX >= 15 or self.posX <= 0)):
+            return False
+        else:
+            return True
+
+
+
+
         
