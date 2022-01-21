e2afdef76a1c0a3e063f8b2f58a77983b38d786f
DE\BOD6SGM <Damian.BOck@bosch.com>
2022-01-21 12:25:20 +0100

    TEST

--- PACCA/main.py
+++ PACCA/main.py
@@ -1,6 +1,6 @@
 import csv
-
-import numpy
+from pathlib import Path
+import numpy as np
 
 
 def show(ausgabe: list):
@@ -9,7 +9,7 @@ def show(ausgabe: list):
         for x in range(length):
             printed = False
             for color in range(3):
-                if(ausgabe[x][y][color] == 255):
+                if(ausgabe[x][y][color] == 91):
                     print("0", end =" ")
                     printed = True
                     break
@@ -18,20 +18,32 @@ def show(ausgabe: list):
                     printed = True
                     break
             if (not printed):
-                print("_", end =" ")
-        print("test")
+                print("/", end =" ")
+        print()
+def movepixelleft(ausgabe,x,y):
+    for color in range(3):
+        if (x!=0):
+            ausgabe[x-1][y][color] = ausgabe[x][y][color]   
+        ausgabe[x][y][color] = 0;
+
+pixelArray = np.full((16 , 16, 3), 0)
+walllocation = [16,28,40,0]
+pixelArray[3][8][0] = 250
+
 def createLevel():
-    basePath = Path(__file__)
-    filePath = (basePath / ("level").resolve())
+    basePath = Path(__file__).parent
+    filePath = (basePath / ("level.csv")).resolve()
 
     with open(filePath) as file:
         sprite = [line for line in csv.reader(file)]
     length = len(sprite)
     for y in range(length):
         for x in range(length):
-            if(sprite[x][y]!=0):
-                pixelArray[x][y][0] = 255
-def main():
-    pixelArray = np.full((16 , 16, 3), 0)
-    show(pixelArray)
-
+            if(int(sprite[x][y])==1):
+                pixelArray[x][y][0] = 91
+                pixelArray[x][y][1] = 58
+                pixelArray[x][y][2] = 41
+pixelArray = np.full((16 , 16, 3), 0)
+show(pixelArray)
+createLevel()
+show(pixelArray)
