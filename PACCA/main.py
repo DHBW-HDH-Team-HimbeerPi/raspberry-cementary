6e4a868fac66be2699b05d0b1b08e6f8312f6feb
LARJAEG <lars.jaeger@daimler.com>
2022-01-28 13:22:07 +0100

    fixing movement and implementing controls

--- PACCA/main.py
+++ PACCA/main.py
@@ -1,43 +1,42 @@
+debug = 1
 import csv
 from pathlib import Path
 import numpy as np
 import time
 import random
-from input_framework.imu_controller import IMUController
-from input_framework.interface import ThresholdType, TriggerMode
 
-
-from output_framework.output_framework import OutputFramework
+if debug != 1:
+    from output_framework.output_framework import OutputFramework
 import os
+
+
 def show(ausgabe: list):
     length = len(ausgabe)
     for y in range(length):
         for x in range(length):
             printed = False
             for color in range(3):
-                if(ausgabe[x][y][color] == 91):
-                    print("0", end =" ")
+                if (ausgabe[x][y][color] == 91):
+                    print("X", end=" ")
                     printed = True
                     break
-                if(ausgabe[x][y][color] == 255):
-                    print("1", end =" ")
+                if (ausgabe[x][y][color] == 255):
+                    print("1", end=" ")
                     printed = True
                     break
             if (not printed):
-                print("/", end =" ")
+                print(" ", end=" ")
         print()
-def movepixelleft(ausgabe,x,y):
-    for color in range(3):
-        if (x!=0):
-            ausgabe[x-1][y][color] = ausgabe[x][y][color]   
-        ausgabe[x][y][color] = 0;
 
-pixelArray = np.full((16 , 16, 3), 0)
+
+pixelArray = np.full((16, 16, 3), 0)
 global lastdir
-lastdir=0
-playerposition=[0,0]
-pixelArray[0][0][0]=255
-pixelArray[0][0][1]=255
+lastdir = 0
+playerposition = [0, 0]
+pixelArray[0][0][0] = 255
+pixelArray[0][0][1] = 255
+
+
 def createLevel():
     basePath = Path(__file__).parent
     filePath = (basePath / ("level.csv")).resolve()
@@ -47,73 +46,90 @@ def createLevel():
     length = len(sprite)
     for y in range(length):
         for x in range(length):
-            if(int(sprite[x][y])==1):
+            if (int(sprite[x][y]) == 1):
                 pixelArray[x][y][0] = 91
                 pixelArray[x][y][1] = 58
                 pixelArray[x][y][2] = 41
+
+
 def moveplayer(dira):
+    global has_been_triggered
+    global lastdir
+    if has_been_triggered != 0:
+        return
+    else:
+        has_been_triggered = 1
     dirb = dira
     moved = True
-    for x in range (2):
+    for x in range(2):
         match dirb:
             case 1:
-                if(playerposition[1]<15 and pixelArray[playerposition[0]][playerposition[1]+1][0]==0):
-                    pixelArray[playerposition[0]][playerposition[1]+1][0]=255
-                    pixelArray[playerposition[0]][playerposition[1]+1][1]=255
-                    pixelArray[playerposition[0]][playerposition[1]][0]=0
-                    pixelArray[playerposition[0]][playerposition[1]][1]=0
-                    playerposition[1]+=1
-                    dirb=0
-                else: 
-                    dirb=lastdir
+                if (playerposition[1] < 15 and pixelArray[playerposition[0]][playerposition[1] + 1][0] == 0):
+                    pixelArray[playerposition[0]][playerposition[1] + 1][0] = 255
+                    pixelArray[playerposition[0]][playerposition[1] + 1][1] = 255
+                    pixelArray[playerposition[0]][playerposition[1]][0] = 0
+                    pixelArray[playerposition[0]][playerposition[1]][1] = 0
+                    playerposition[1] += 1
+                    dirb = 0
+                    lastdir = dira
+                else:
+                    dirb = lastdir
             case 2:
-                if(playerposition[1]>0 and pixelArray[playerposition[0]][playerposition[1]-1][0]==0):
-                    pixelArray[playerposition[0]][playerposition[1]-1][0]=255
-                    pixelArray[playerposition[0]][playerposition[1]-1][1]=255
-                    pixelArray[playerposition[0]][playerposition[1]][0]=0
-                    pixelArray[playerposition[0]][playerposition[1]][1]=0
-                    playerposition[1]-=1
-                    dirb=0
-                else: 
-                    dirb=lastdir
+                if (playerposition[1] > 0 and pixelArray[playerposition[0]][playerposition[1] - 1][0] == 0):
+                    pixelArray[playerposition[0]][playerposition[1] - 1][0] = 255
+                    pixelArray[playerposition[0]][playerposition[1] - 1][1] = 255
+                    pixelArray[playerposition[0]][playerposition[1]][0] = 0
+                    pixelArray[playerposition[0]][playerposition[1]][1] = 0
+                    playerposition[1] -= 1
+                    dirb = 0
+                    lastdir = dira
+                else:
+                    dirb = lastdir
             case 3:
-                if(playerposition[0]<15 and pixelArray[playerposition[0+1]][playerposition[1]][0]==0):
-                    pixelArray[playerposition[0+1]][playerposition[1]][0]=255
-                    pixelArray[playerposition[0+1]][playerposition[1]][1]=255
-                    pixelArray[playerposition[0]][playerposition[1]][0]=0
-                    pixelArray[playerposition[0]][playerposition[1]][1]=0
-                    playerposition[0]+=1
-                    dirb=0
+                if (playerposition[0] < 15 and pixelArray[playerposition[0] + 1][playerposition[1]][0] == 0):
+                    pixelArray[playerposition[0] + 1][playerposition[1]][0] = 255
+                    pixelArray[playerposition[0] + 1][playerposition[1]][1] = 255
+                    pixelArray[playerposition[0]][playerposition[1]][0] = 0
+                    pixelArray[playerposition[0]][playerposition[1]][1] = 0
+                    playerposition[0] += 1
+                    dirb = 0
+                    lastdir = dira
                 else:
-                    dirb=lastdir
+                    dirb = lastdir
             case 4:
-                if(playerposition[0]>0 and pixelArray[playerposition[0-1]][playerposition[1]][0]==0):
-                    pixelArray[playerposition[0-1]][playerposition[1]][0]=255
-                    pixelArray[playerposition[0-1]][playerposition[1]][1]=255
-                    pixelArray[playerposition[0]][playerposition[1]][0]=0
-                    pixelArray[playerposition[0]][playerposition[1]][1]=0
-                    playerposition[0]-=1
-                    dirb=0
+                if (playerposition[0] > 0 and pixelArray[playerposition[0] - 1][playerposition[1]][0] == 0):
+                    pixelArray[playerposition[0] - 1][playerposition[1]][0] = 255
+                    pixelArray[playerposition[0] - 1][playerposition[1]][1] = 255
+                    pixelArray[playerposition[0]][playerposition[1]][0] = 0
+                    pixelArray[playerposition[0]][playerposition[1]][1] = 0
+                    playerposition[0] -= 1
+                    dirb = 0
+                    lastdir = dira
                 else:
-                    dirb=lastdir
-            case _: 
-                dirb=lastdir
+                    dirb = lastdir
+            case _:
+                dirb = lastdir
 
-
-
-
-
-show(pixelArray)
 createLevel()
 show(pixelArray)
+os.system('cls')
+
+ctrl = IMUController()
+threshold = 0.35
+ctrl.register_trigger(moveplayer, {'dira' : 1 }, ctrl.mov_x, threshold, ThresholdType.HIGHER)
+ctrl.register_trigger(moveplayer, {'dira' : 2 }, ctrl.mov_x, -threshold, ThresholdType.LOWER)
+ctrl.register_trigger(moveplayer, {'dira' : 3 }, ctrl.mov_y, threshold, ThresholdType.HIGHER)
+ctrl.register_trigger(moveplayer, {'dira' : 4 }, ctrl.mov_y, -threshold, ThresholdType.LOWER)
+has_been_triggered = 0
 
 for x in range(100):
-    time.sleep(0.1)
-    moveplayer(random.randrange(4)+1)
-    time.sleep(0.1)
-    #os.system('cls')
-    #ctrl.check_triggers()
-    OutputFramework.setWindow(pixelArray)
-    #show(pixelArray)
-    
 
+    has_been_triggered = 0
+    time.sleep(0.1)
+    moveplayer(random.randrange(4) + 1)
+    time.sleep(0.1)
+    os.system('cls')
+    ctrl.check_triggers()
+    if debug != 1:
+        OutputFramework.setWindow(pixelArray)
+    show(pixelArray)
