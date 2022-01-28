69f558df9dceb910e8af4c9916bb9786db1c7f0d
tinvol <joe@biden.com>
2022-01-28 13:23:16 +0100

    xddddddd

--- sidescroller/src/input.py
+++ sidescroller/src/input.py
@@ -3,9 +3,9 @@ from src.shiftPixels import shiftPlayerDown, shiftPlayerUp, walkRight, walkLeft
 
 def inputToDirection(dir, pixelArray):
     if(dir == Directions.up.value):
-        shiftPlayerDown(pixelArray)
-    elif(dir == Directions.down.value):
         shiftPlayerUp(pixelArray)
+    elif(dir == Directions.down.value):
+        shiftPlayerDown(pixelArray)
     elif(dir == Directions.left.value):
         walkLeft(pixelArray)
     else:
