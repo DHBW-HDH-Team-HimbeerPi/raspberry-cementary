20fb61e0dc84b1405a088d9942ced71aca94763d
DE\BOD6SGM <Damian.BOck@bosch.com>
2022-01-28 11:37:19 +0100

    Test15

--- FLAPPA/main.py
+++ FLAPPA/main.py
@@ -96,8 +96,8 @@ def movepixelleft(ausgabe,x,y):
 
 
 ctrl = IMUController(TriggerMode.CALL_CHECK)
-ctrl.register_trigger(playermovementv, {'velocity' : 1 }, ctrl.rot_x, 0.8, ThresholdType.HIGHER)
-ctrl.register_trigger(playermovementv, {'velocity' : -1 }, ctrl.rot_x, -0.8, ThresholdType.LOWER)
+ctrl.register_trigger(playermovementv, {'velocity' : 1 }, ctrl.rot_x, 0.35, ThresholdType.HIGHER)
+ctrl.register_trigger(playermovementv, {'velocity' : -1 }, ctrl.rot_x, -0.35, ThresholdType.LOWER)
 pixelArray = np.full((16 , 16, 3), 0)
 walllocation = [16,28,40,0,8,0]
 pixelArray[3][8][0] = 250
