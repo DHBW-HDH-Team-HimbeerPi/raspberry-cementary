34e1ae956e1c7d584f38c5d05d0da22cb4a9426c
tinvol <maximilian.volk@daimler.com>
2022-01-28 10:39:39 +0100

    test4

--- sidescroller/src/sanitizePixelArray.py
+++ sidescroller/src/sanitizePixelArray.py
@@ -2,7 +2,7 @@ import numpy as np
 
 def sanatizeArray(pixelArray):
     newPixelArray = np.full((16, 16, 4), 0)
-    for x in range(0, len(pixelArray[0])):
-        for y in range(1, len(pixelArray[0])):
+    for x in range(0, len(newPixelArray[0])):
+        for y in range(1, len(newPixelArray[0])):
             newPixelArray[x][y] = pixelArray[x][y]
     return newPixelArray
