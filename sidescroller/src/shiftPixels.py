383c3bd4df2bf88e8c1c2d41f7ebb370fff37f64
tinvol <joe@yourmom.com>
2022-01-28 11:44:28 +0100

    test16

--- sidescroller/src/shiftPixels.py
+++ sidescroller/src/shiftPixels.py
@@ -2,14 +2,14 @@
 def shiftPixelsY(pixelArray: list):
     for x in range(len(pixelArray)):
         for y in range(1, len(pixelArray[0])):
-            if (pixelArray[x][y-1][3] != 1 and pixelArray[x][y][3] != 1):
+            if (pixelArray[x][y-1][3] == 0 and pixelArray[x][y][3] == 0):
                 pixelArray[x][y-1] = pixelArray[x][y]
     return True
 
 def shiftPlayerUp(pixelArray: list):
     for x in range(16):
         for y in range(1, 16):
-            
+
             if (pixelArray[x][y][3] == 1):
                 pixelArray[x-1][y] = pixelArray[x][y]
                 pixelArray[x][y][0] = 0  
