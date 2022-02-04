752c3b3e9c05aa17d6b15f413ba46e5f52db7c57
DE\BOD6SGM <Damian.BOck@bosch.com>
2022-02-04 11:38:28 +0100

    walllocation with pipelocation

--- FLAPPA/main.py
+++ FLAPPA/main.py
@@ -6,7 +6,8 @@ import os
 from input_framework.imu_controller import IMUController
 from input_framework.interface import ThresholdType, TriggerMode
 global difficulty
-
+global playerposition
+global score
 from output_framework.output_framework import OutputFramework 
 
 
@@ -28,63 +29,65 @@ def show(ausgabe: list):
                 print("_", end =" ")
         print("")
 def checkalive(pixelArray):
-    if(walllocation[0]==3 or walllocation[1]==3 or walllocation[2]==3): 
-        for y in range (15):
-            if(pixelArray[3][y][0]==0):
-                if(walllocation[4]>=y and walllocation[4]<=y+difficulty+1):
-                    return True
-                else:
-                    return False
-        return True
-
-def playermovementv(velocity):
-    walllocation[5]=velocity
+    global playerposition
+    for x in range (3):
+        if(walllocation[x]==3 ): 
+            for y in range (15):
+                    if(playerposition>walllocation[x*2+3] and playerposition<walllocation[x*2+4]):
+                        return True
+                    else:
+                        return False
+            return True
 
 
 
 def playermovement(velocity):
-    pixelArray[3][walllocation[4]][0] = 0
-    walllocation[4]+=velocity
-    if (walllocation[4] > 15 ):
-        walllocation[4]=0
-    if (walllocation[4] < 0 ):
-        walllocation[4]=15;
-    pixelArray[3][walllocation[4]][0] = 250
+    pixelArray[3][playerposition][0] = 0
+    playerposition+=velocity
+    if (playerposition > 15 ):
+        playerposition=0
+    if (playerposition < 0 ):
+        playerposition=15;
+    pixelArray[3][playerposition][0] = 250
 
 
 def movewall(pixelArray,wallocation):
     for walls in range (3):
         if (walllocation[walls] == 0):
             walllocation[walls]= 36
-            walllocation[3]=walllocation[3] + 1
-            print(walllocation[3])
+            score=score + 1
+            print(score)
             movewallleft(pixelArray,0)
             
         elif(walllocation[walls] == 16):
-            createwall(pixelArray)
+            createwall(pixelArray,walls)
             walllocation[walls] =15
         elif(walllocation[walls]<16):
             movewallleft(pixelArray,walllocation[walls])
             walllocation[walls]-=1
         else:
             walllocation[walls]-=1
-def createwall(pixelArray):
+def createwall(pixelArray,walls):
     r = random.randrange(11)
     i=0
     global difficulty   
-    if walllocation[3]>10:
+    if score>10:
         difficulty = 2
     else:
         difficulty = 3
     print (difficulty)
-    print(walllocation[3])
+    print(score)
+    walllocation[x*2+3]=-1
     while i <r :
         pixelArray[15][i][1]=255
         i+=1
+        walllocation[walls*2+3]=i
     i=15
+    walllocation[walls*2+4]=-1
     while i>r+difficulty:
         pixelArray[15][i][1]=255
         i-=1
+        walllocation[x*2+4]=i
 def movewallleft(pixelArray,wall):
     for y in range(16):
         if (pixelArray[wall][y][1]!=0):
@@ -101,19 +104,22 @@ ctrl = IMUController(TriggerMode.CALL_CHECK)
 ctrl.register_trigger(playermovement, {'velocity' : 1 }, ctrl.mov_x, 0.35, ThresholdType.HIGHER)
 ctrl.register_trigger(playermovement, {'velocity' : -1 }, ctrl.mov_x, -0.35, ThresholdType.LOWER)
 pixelArray = np.full((16 , 16, 3), 0)
-walllocation = [16,28,40,0,8,0]
+walllocation = [16,28,40,0,0,0,0,0,0]
+
+global playerposition 
+playerpostion =8
 pixelArray[3][8][0] = 250
 for u in range (240):
     
     movewall(pixelArray,walllocation)
     OutputFramework.setWindow(pixelArray)
     ctrl.check_triggers()
-    time.sleep(0.1/math.log(walllocation[3]+2,15)/8)
+    time.sleep(0.1/math.log(score+2,15)/8)
     if (checkalive(pixelArray)==False):
         break
     OutputFramework.setWindow(pixelArray)
     ctrl.check_triggers()
-    time.sleep(0.1/math.log(walllocation[3]+2,15)/8)
+    time.sleep(0.1/math.log(score+2,15)/8)
     OutputFramework.setWindow(pixelArray)
     #show(pixelArray)
     if (checkalive(pixelArray)==False):
