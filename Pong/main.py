b7f0d23d190383e697b60a6bdf83b5614eeb64cd
Snerksss <berndts.tin21@student.dhbw-heidenheim.de>
2022-02-04 12:58:00 +0100

    ball update

--- Pong/main.py
+++ Pong/main.py
@@ -1,10 +1,10 @@
-from ball import Ball
-from panel import Panel
+import ball
+import comPlayer
+import panel
 from output_framework.output_framework import OutputFramework as oF
 from input_framework.imu_controller import IMUController
 from input_framework.interface import ThresholdType, TriggerMode
 # from unicornhatsimulator import unicornhathd as uni
-from comPlayer import aiPlayer
 import numpy as np
 import time
 
@@ -12,13 +12,12 @@ import time
 class Pong:
 
     def __init__(self):
-        self.leftPanel = Panel(1, 0, 0, 255)
-        self.rightPanel = Panel(14, 0, 255, 0)
-        self.gameBall = Ball()
+        self.leftPanel = panel.Panel(1, 0, 0, 255)
+        self.rightPanel = panel.Panel(14, 0, 255, 0)
+        self.gameBall = ball.Ball()
         self.scoreLeft = 0
         self.scoreRight = 0
-        self.com = aiPlayer()
-        self.com2 = aiPlayer()
+        self.com = comPlayer.aiPlayer()
 
         self.play()
 
