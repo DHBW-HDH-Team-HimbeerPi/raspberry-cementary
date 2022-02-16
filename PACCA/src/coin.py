a5200738e08895a0345dbd57a7f065bebf6d1a02
DE\BOD6SGM <Damian.BOck@bosch.com>
2022-02-16 12:02:06 +0100

    TetsC

--- PACCA/src/coin.py
+++ PACCA/src/coin.py
@@ -3,7 +3,7 @@ import random
 class Coin():
 
     def __init__(self,pixelArray):
-        while(true):
+        while(True):
             x = random.randrange(16)
             y = random.randrange(16)
             if(pixelArray[x][y][1]==0):
@@ -12,5 +12,5 @@ class Coin():
             
     def showcoin(self,pixelArray):
         pixelArray[self.posxy[0]][self.posxy[1]][0]=255
-        pixelArray[self.posxy[0]][self.posxy[1]][1]=211
-        pixelArray[self.posxy[0]][self.posxy[1]][2]=62
+        pixelArray[self.posxy[0]][self.posxy[1]][1]=53
+        pixelArray[self.posxy[0]][self.posxy[1]][2]=180
