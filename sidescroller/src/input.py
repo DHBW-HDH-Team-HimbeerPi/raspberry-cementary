1c2c0e4377c49fefc2b635a59c8588f07fd81e67
tinvol <joe@biden.com>
2022-02-16 11:50:08 +0100

    sheeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeesh

--- sidescroller/src/input.py
+++ sidescroller/src/input.py
@@ -1,4 +1,6 @@
 from enum import Enum
+
+from .frameBuffer import FrameBuffer
 from .player import Player
 
 class Directions(Enum):
@@ -7,7 +9,7 @@ class Directions(Enum):
     left = 3
     right = 4
 
-def inputToDirection(dir, pixelArray, player: Player):
+def inputToDirection(dir, pixelArray, player: Player, frameBuffer: FrameBuffer):
     if dir == Directions.right.value:
         player.walkRight(pixelArray)
         print("right")
@@ -15,7 +17,7 @@ def inputToDirection(dir, pixelArray, player: Player):
         player.walkLeft(pixelArray)
         print("left")
     elif dir == Directions.up.value:
-        player.jump(pixelArray)
+        player.jump(pixelArray, frameBuffer)
         print("up")
     else:
         print("down")
