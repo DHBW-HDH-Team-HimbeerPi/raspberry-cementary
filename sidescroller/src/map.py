6a4bd89d1197b000a307174984cfc8d880841fe0
tinvol <joe@biden.com>
2022-02-19 17:45:25 +0100

    xd

--- sidescroller/src/map.py
+++ sidescroller/src/map.py
@@ -27,12 +27,14 @@ class Map:
                         yPos -= grid
                     
                     if yPos > 7:
-                        if self.movedPixels % 32 == 0:
-                            yPos = int(yPos-4*(self.movedPixels/32))
-                        else:
-                            yPos = int(yPos-4*(self.movedPixels-16)/32)
-                            if yPos > 7:
-                                yPos -= 4
+                        while yPos > 7:
+                            yPos = yPos-4
+                        #if self.movedPixels % 32 == 0:
+                        #    yPos = int(yPos-4*(self.movedPixels/32))
+                        #else:
+                        #    yPos = int(yPos-4*(self.movedPixels-16)/32)
+                        #    if yPos > 7:
+                        #        yPos -= 4
                         #yPos = y-int(8*(self.movedPixels-32)/16)
                         #if yPos == 0:
                         #    yPos += grid
