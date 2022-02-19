7457fab4d34b3f4e6ebae97c4b492abe5d9bdc0a
tinvol <joe@biden.com>
2022-02-19 14:15:48 +0100

    khgwkfkwfkjwölwklgw

--- sidescroller/src/input.py
+++ sidescroller/src/input.py
@@ -12,7 +12,7 @@ class Directions(Enum):
 def inputToDirection(dir, pixelArray, player: Player, frameBuffer: FrameBuffer, map: Map):
         
     if player.goingDownPossible(pixelArray) and not player.isJumping and not frameBuffer.running:
-        player.shiftPlayerDown(pixelArray)
+        #player.shiftPlayerDown(pixelArray)
         #player.shiftPlayerDown(pixelArray)
 
     if player.posX < 13:
