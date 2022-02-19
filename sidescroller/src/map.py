515ddf3cfc7ff20fad084c323da5c1572e9bd033
tinvol <joe@biden.com>
2022-02-19 17:42:47 +0100

    xd

--- sidescroller/src/map.py
+++ sidescroller/src/map.py
@@ -31,7 +31,7 @@ class Map:
                             yPos = int(yPos-4*(self.movedPixels/32))
                         else:
                             yPos = int(yPos-4*(self.movedPixels-16)/32)
-                            if yPos == 8:
+                            if yPos > 7:
                                 yPos -= 4
                         #yPos = y-int(8*(self.movedPixels-32)/16)
                         #if yPos == 0:
